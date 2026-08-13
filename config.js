adcd409ff0ac88bee6c8716571f47a38f7f8cf9d
fhdhdhfjfgdjdj-coder <fhdhdhfjfgdjdj@gmail.com>
2026-08-13 16:53:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,9 @@
  */
 
 const CONFIG = {
-  // Google Apps Script Web App URL
-  API_URL: "https://script.google.com/macros/s/AKfycbw9pOux-Evvi5kRSR2AvlW5yHTZDMhnzykWRz_tHM9o6XNLe2BqGD1HjxTDzpsZ5A4X/exec",
-
+  
   // Discord Webhook URL
-  DISCORD_WEBHOOK_URL: "https://discordapp.com/api/webhooks/1536659596763070526/EV0yxPShYa1YP4N6HsDdQbKdqlVf4rbBqJk3aAUIe9jAy2MlpyHdkVQmv4z0Y9gs49M3",
+  DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1536659596763070526/EV0yxPShYa1YP4N6HsDdQbKdqlVf4rbBqJk3aAUIe9jAy2MlpyHdkVQmv4z0Y9gs49M3",
 
   // วาง Channel Access Token ที่ก๊อปปี้มาจากแท็บ Messaging API
   LINE_CHANNEL_ACCESS_TOKEN: "ZnyvoMCmDT+opgRS7P2VsjCnLeriK3i3QNyMeO+tyAaHYRf8ARX8+5Uf2J3unyWFTMP0ugFOCKbG7ZmVDssect7wmvePIJrBgUd6SpULhJ5VLHLF6CYsuHteq5LCW+h3olUwI9pyBIMNUni8eilOjAdB04t89/1O/w1cDnyilFU=",

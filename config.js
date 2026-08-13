30e499474d27669673ec4bfa7eaf024ed1950b22
fhdhdhfjfgdjdj-coder <fhdhdhfjfgdjdj@gmail.com>
2026-08-13 17:15:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,8 @@
  */
 
 const CONFIG = {
+  // 1. ลิงก์ Web App จาก Google Apps Script (ที่สั่ง Deploy เป็น Web App)
+  API_URL: "https://script.google.com/macros/s/AKfycbxwxxPenxpdVRL-dIUTCgylqB9Pgr3Nbq-xiBkpX6rTnQsXhw9d_SfaY78l7GMxxruO7g/exec",
   
   // Discord Webhook URL
   DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1536659596763070526/EV0yxPShYa1YP4N6HsDdQbKdqlVf4rbBqJk3aAUIe9jAy2MlpyHdkVQmv4z0Y9gs49M3",

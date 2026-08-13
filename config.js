d43949e40f216367651d85ce38950146b0b2bd18
fhdhdhfjfgdjdj-coder <fhdhdhfjfgdjdj@gmail.com>
2026-08-13 16:15:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
   // Google Apps Script Web App URL (ใช้ URL ล่าสุดที่คุณเพิ่ง Deploy)
   API_URL: "https://script.google.com/macros/s/AKfycbzhwnOCr-0xyRbQ7oNpq9GHVcYXVKU2V6xxJ8pcb9XGpWfu4bvjSywZVOxkISROl-zr/exec",
-
+  
   // Discord Webhook URL
   DISCORD_WEBHOOK_URL: "https://discordapp.com/api/webhooks/1536659596763070526/EV0yxPShYa1YP4N6HsDdQbKdqlVf4rbBqJk3aAUIe9jAy2MlpyHdkVQmv4z0Y9gs49M3",
 

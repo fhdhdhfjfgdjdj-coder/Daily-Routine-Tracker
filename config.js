7b5fd0908dd0e889a705298c27130f10dcda4fdf
fhdhdhfjfgdjdj-coder <fhdhdhfjfgdjdj@gmail.com>
2026-08-17 10:47:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   // วาง URL ของเว็บแอปที่ได้จาก Google Apps Script หลัง Deploy > New deployment
-  API_URL: "https://script.google.com/macros/s/AKfycbz0ZUw38IYYAzCepRO0LGcH2eVJiAwAOlRiYGDYm9Q/dev",
+  API_URL: "https://script.google.com/macros/s/AKfycbwA8GaoSOdZIGcCU7TH_KIfcZ9Jx64PFx-oiYr8YtWT/dev",
   
     // Discord Webhook URL
   DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1536659596763070526/EV0yxPShYa1YP4N6HsDdQbKdqlVf4rbBqJk3aAUIe9jAy2MlpyHdkVQmv4z0Y9gs49M3",
